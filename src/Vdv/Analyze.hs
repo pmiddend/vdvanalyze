7da10d1f6fda205f797816d0a54f39457719c1c0
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-07-12 15:00:49 +0200

    Skeleton functions

---
+++ src/Vdv/Analyze.hs
@@ -0,0 +1,53 @@
+module Main where
+
+import Text.XML.Lens(Element)
+import ClassyPrelude
+
+newtype Logfile = Logfile Text
+
+newtype DataMessage = DataMessage Element
+
+newtype TagName = TagName Text
+
+newtype Journey = Journey Element
+
+prettyPrintDataMessage :: DataMessage -> DataMessage
+prettyPrintDataMessage = error "prettyPrintDataMessage not implemented"
+
+readLogfile :: MonadIO m => FilePath -> m Logfile
+readLogfile = error "readLogfile not implemented"
+
+extractTags :: Text -> TagName -> [Text]
+extractTags = error "extractTags not implemented"
+
+extractDataMessages :: Logfile -> [DataMessage]
+extractDataMessages = error "extractDataMessages not implemented"
+
+extractJourneys :: DataMessage -> [Journey]
+extractJourneys = error "extractJourneys not implemented"
+
+extractZst :: DataMessage -> Text
+extractZst = error "extractZst not implemented"
+
+outputDataMessages :: MonadIO m => [DataMessage] -> m ()
+outputDataMessages = error "outputDataMessages not implemented"
+
+filterJourney :: Filter -> Journey -> Bool
+filterJourney = error "filterJourney not implemented"
+
+data Filter = Filter {
+    _filterTagName :: Text
+  , _filterTagValue :: Text
+  }
+
+parseFilters :: Text -> [Filter]
+parseFilters = error "parseFilters not implemented"
+
+data Settings = Settings {
+    _settingsInputFile :: FilePath
+  , _settingsFilters :: [Filter]
+  , _settingsInvert :: Bool
+  } deriving(Show,Eq)
+
+main :: IO ()
+main = return ()
